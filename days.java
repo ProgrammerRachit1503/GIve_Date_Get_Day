cf6fb58a62b6afdf778fbec9fcb69e6d2d4eafd4
Rachit Jain <108483070+ProgrammerRachit1503@users.noreply.github.com>
2022-07-08 01:54:57 +0530

    Update days.java

--- days.java
+++ days.java
@@ -1,13 +1,9 @@
 import jdk.swing.interop.SwingInterOpUtils;
-
 import javax.imageio.metadata.IIOInvalidTreeException;
 import java.util.*;
-
-
 public class days {
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
-
         int[] numDays = {31,28,31,30,31,30,31,31,30,31,30,31};
         System.out.println("a month, day and year in number form");
         int month = kb.nextInt();
@@ -16,10 +12,8 @@ public class days {
         kb.nextLine();
         int year = kb.nextInt();
         kb.nextLine();
-
         int wasteYears = (year-1)%400;
         int wasteDays = 0;
-
         while(wasteYears != 0){
             if(wasteYears >= 100){
                 wasteDays += 5;
@@ -46,12 +40,6 @@ public class days {
         }
         wasteDays = (wasteDays + monthDays + day)%7;
         String[] daysOfTheWeek = {"Sunday" , "Monday" , "Tuesday" , "Wednesday" , "Thursday " , "Friday" , "Saturday"};
-
         System.out.println(daysOfTheWeek[wasteDays]);
-
-
-
-
     }
-
 }
